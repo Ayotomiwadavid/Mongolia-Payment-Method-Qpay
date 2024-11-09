e67f0e6c84b8c4afe975d3cc8d71f575584b3ff7
Ayotomiwadavid <ayotomiwadavid@proton.me>
2024-11-09 17:04:48 +0100

    saving progress

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ const express = require('express');
 const bodyParser = require("body-parser");
 const axios = require('axios');
 const WebSocket = require('ws');
-const QPAY = require('@togtokh.dev/qpay');
 require('dotenv').config();
 
 const app = express();
